b00e9831e4f588b9040ca10ff0996249482827bb
reaper47 <macpoule@gmail.com>
2021-06-13 22:48:09 +0300

    updated webpack to v5

--- web/app/.eslintrc.js
+++ web/app/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: '@babel/eslint-parser',
   },
   rules: {
     "no-console": "off",
--- web/app/src/components/SearchForm.vue
+++ web/app/src/components/SearchForm.vue
@@ -1,6 +1,7 @@
 <template>
   <div></div>
 </template>
+
 <script>
 export default {
   data() {
