7afcaf94dafee4c85cf345a945e67797b9e95c16
reaper47 <macpoule@gmail.com>
2021-06-14 13:47:08 +0300

    added vuetify

--- web/app/src/main.js
+++ web/app/src/main.js
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+
   render: function (h) {
     return h(App);
   },
--- web/app/vue.config.js
+++ web/app/vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   productionSourceMap: false,
+
+  transpileDependencies: ["vuetify"],
 };
