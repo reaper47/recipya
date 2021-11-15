1faf629c02ee5dde58d5b03b3455310ef416436a
reaper47 <macpoule@gmail.com>
2021-11-16 00:11:57 +0100

    remove gitignore files

--- old/tools/venv/lib/python3.9/site-packages/extruct/__main__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-from extruct.tool import main
-
-
-if __name__ == '__main__':
-    print(main())
--- old/tools/venv/lib/python3.9/site-packages/nuitka/build/inline_copy/clcache/clcache/__init__.py
+++
@@ -1,4 +0,0 @@
-""" clcache package """
-
-from __future__ import absolute_import
-from .caching import VERSION
