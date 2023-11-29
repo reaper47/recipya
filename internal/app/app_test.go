7e3df31021afbc71fd805b7b830af0f49d635acc
reaper47 <macpoule@gmail.com>
2023-11-29 17:24:13 +0100

    Fix server URL address when in production (#130)

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -33,6 +33,11 @@ func TestConfigFile_Address(t *testing.T) {
 			in:   app.ConfigFile{Server: app.ConfigServer{URL: "https://127.0.0.1", Port: 8078}},
 			want: "https://" + net.JoinHostPort(ip, "8078"),
 		},
+		{
+			name: "hosted somewhere",
+			in:   app.ConfigFile{Server: app.ConfigServer{URL: "https://recipya.com", Port: 8078, IsProduction: true}},
+			want: "https://recipya.com",
+		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
