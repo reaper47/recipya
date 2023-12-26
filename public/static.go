cea3053422d279dd54baa4d76c0f94200b8ded99
reaper47 <macpoule@gmail.com>
2023-12-26 19:20:44 +0100

    remove unused public directory

--- public/static.go
+++
@@ -1,9 +0,0 @@
-package static
-
-import "embed"
-
-// FS is a read-only embedded file system containing static web assets.
-// It includes CSS, images, and other static files needed by the application.
-//
-//go:embed css img favicon.ico robots.txt
-var FS embed.FS
