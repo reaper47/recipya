e7fb68be0c9001fbb6915e953098d9fc7f432135
Dan Simpson <daniel_simpson1988@hotmail.com>
2024-02-23 22:45:15 +1100

    Maybe fix up some test mocks :sweat_smile:

--- internal/server/server_mock_test.go
+++ internal/server/server_mock_test.go
@@ -863,6 +863,10 @@ func (m *mockFiles) UploadImage(rc io.ReadCloser) (uuid.UUID, error) {
 	return uuid.New(), nil
 }
 
+func (m *mockFiles) ScrapeAndStoreImage(rawURL string) (string, error) {
+	return uuid.New().String(), nil
+}
+
 type mockIntegrations struct {
 	NextcloudImportFunc func(baseURL, username, password string, files services.FilesService) (*models.Recipes, error)
 	ProcessImageOCRFunc func(file io.Reader) (models.Recipe, error)
