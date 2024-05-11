27b57283ca9f9f0a32525839f54f4f6e7c592476
reaper47 <macpoule@gmail.com>
2024-05-11 19:06:51 +0200

    Improve parsing of maangchi.com

--- internal/scraper/maangchi.go
+++ internal/scraper/maangchi.go
@@ -3,6 +3,7 @@ package scraper
 import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/reaper47/recipya/internal/models"
+	"slices"
 	"strings"
 )
 
@@ -12,25 +13,57 @@ func scrapeMaangchi(root *goquery.Document) (models.RecipeSchema, error) {
 		return rs, err
 	}
 
-	if len(rs.Ingredients.Values) == 0 {
-		nodes := root.Find("h2:contains('Ingredients') + ul li")
-		rs.Ingredients.Values = make([]string, 0, nodes.Length())
-		nodes.Each(func(_ int, sel *goquery.Selection) {
-			rs.Ingredients.Values = append(rs.Ingredients.Values, sel.Text())
-		})
+	node := root.Find("h2:contains('Ingredients')")
+	if len(node.Nodes) > 0 {
+		clear(rs.Ingredients.Values)
+
+		for c := node.Nodes[0].NextSibling; c != nil; c = c.NextSibling {
+			if c.Data == "h2" && strings.ToLower(c.FirstChild.Data) == "directions" {
+				break
+			}
+
+			switch c.Data {
+			case "h3":
+				rs.Ingredients.Values = append(rs.Ingredients.Values, strings.TrimSpace(c.FirstChild.Data))
+			case "ul":
+				goquery.NewDocumentFromNode(c).Find("li").Each(func(_ int, sel *goquery.Selection) {
+					rs.Ingredients.Values = append(rs.Ingredients.Values, strings.TrimSpace(sel.Text()))
+				})
+			}
+		}
 	}
 
-	if len(rs.Instructions.Values) == 0 {
-		nodes := root.Find("h2:contains('Directions') + ol li")
-		rs.Instructions.Values = make([]string, 0, nodes.Length())
-		nodes.Each(func(_ int, sel *goquery.Selection) {
-			s := sel.Text()
-			before, _, ok := strings.Cut(s, "<img")
-			if ok {
-				s = before
+	node = root.Find("h2:contains('Directions')")
+	if node != nil {
+		clear(rs.Instructions.Values)
+
+		for c := node.Nodes[0].NextSibling; c != nil; c = c.NextSibling {
+			if c.Data == "h2" && strings.ToLower(c.FirstChild.Data) == "directions" {
+				break
 			}
-			rs.Instructions.Values = append(rs.Instructions.Values, s)
-		})
+
+			switch c.Data {
+			case "h3":
+				rs.Instructions.Values = append(rs.Instructions.Values, strings.TrimSpace(c.FirstChild.Data))
+			case "p":
+				s := strings.TrimSpace(c.FirstChild.Data)
+				if s != "a" {
+					rs.Instructions.Values = append(rs.Instructions.Values, s)
+				}
+			case "ol":
+				goquery.NewDocumentFromNode(c).Find("li").Each(func(_ int, sel *goquery.Selection) {
+					s := sel.Text()
+					before, _, ok := strings.Cut(s, "<img")
+					if ok {
+						s = before
+					}
+					rs.Instructions.Values = append(rs.Instructions.Values, strings.TrimSpace(s))
+				})
+			}
+		}
 	}
+
+	rs.Ingredients.Values = slices.DeleteFunc(rs.Ingredients.Values, func(s string) bool { return s == "" })
+	rs.Instructions.Values = slices.DeleteFunc(rs.Instructions.Values, func(s string) bool { return s == "" })
 	return rs, nil
 }
