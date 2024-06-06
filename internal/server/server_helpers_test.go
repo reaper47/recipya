b01384ef2004c9de288ce321f3dd6a06570485c7
reaper47 <macpoule@gmail.com>
2024-06-06 10:38:54 +0200

    Fix Docker image (#343)

--- internal/server/server_helpers_test.go
+++ internal/server/server_helpers_test.go
@@ -52,7 +52,7 @@ func createWSServer() (*server.Server, *httptest.Server, *websocket.Conn) {
 	ts := httptest.NewServer(srv.Router)
 	u := strings.Replace(ts.URL, "http", "ws", 1)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	c, _, err := websocket.Dial(ctx, u+"/ws", &websocket.DialOptions{HTTPHeader: h})
@@ -235,7 +235,7 @@ func getBodyHTML(rr *httptest.ResponseRecorder) string {
 
 func readMessage(tb testing.TB, c *websocket.Conn, number int) (websocket.MessageType, []byte) {
 	tb.Helper()
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	var (
